8847838d7ea79139dfb1fdc658ec38d2ce479d8e
asim.morpho <asim.morpho@gmail.com>
2026-06-20 11:45:45 +0000

    add

--- terraformfiles/provider.tf
+++ terraformfiles/provider.tf
@@ -8,10 +8,10 @@ terraform {
 
 
   backend "azurerm" {
-    resource_group_name = "rg-terraform-state"
+    resource_group_name  = "rg-terraform-state"
     storage_account_name = "stntmstfstate"
-    container_name = "asim" #replace with your name
-    key = "asim.terraform.tfstate" #replace with your name
+    container_name       = "asim"                   #replace with your name
+    key                  = "asim.terraform.tfstate" #replace with your name
   }
 }
 
