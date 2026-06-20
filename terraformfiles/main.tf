64371dbdbd0a448021a9c1cdc011afa73691a4cc
asim.morpho <asim.morpho@gmail.com>
2026-06-20 10:08:01 +0000

    add

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,20 +1,17 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "ayushi-rg-cicd-ntms-workshop"
+  name     = "asim-rg-cicd-ntms-workshop"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "ayushi-vnet-cicd-workshop"
+  name                = "asim-vnet-cicd-workshop"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.1.0.0/16"]
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "web-subnet"
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  resource_group_name  = azurerm_resource_group.rg.name
-}
+
+
 resource "azurerm_subnet" "subnet" {
   name                 = "web-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
